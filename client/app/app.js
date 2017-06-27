abae4d033fec1848da667c785823cc98bb77c795
ItamarGronich <ItamarGronich@Gmail.com>
2017-06-27 09:59:30 +0300

    Remove unnecessary files

--- client/app/app.js
+++ client/app/app.js
@@ -4,11 +4,9 @@ import './app.scss';
 import angular from 'angular';
 
 // Import base modules
-import config from './app.config';
 import appConstants from 'appConstants';
 
 
 export default angular.module('fundbox-test', [])
-  .config(config)
   .constant("AppConstants", appConstants)
   .name;
