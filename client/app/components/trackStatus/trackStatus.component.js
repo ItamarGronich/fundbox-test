deeace25043ed84ce751e32c2bcd73481e931aa6
ItamarGronich <ItamarGronich@Gmail.com>
2017-06-29 15:20:44 +0300

    Added background gradient

--- client/app/components/trackStatus/trackStatus.component.js
+++ client/app/components/trackStatus/trackStatus.component.js
@@ -7,20 +7,22 @@ export default {
     <section class="TrackStatus">
       <header class="TrackStatus-header">
         <span> {{ctrl.track.track_title}} </span>
-        <span> {{ctrl.track.artist_name}}</span>
+        <span> {{ctrl.track.artist_name}} </span>
       </header>
 
-      <p>
+      <p class="TrackStatus-progressWrapper">
         <input
+        class="TrackStatus-progress"
         type="range"
         min="0"
         max="100"
         ng-model="ctrl.progressPercentage"
         ng-mouseup="ctrl.seek()"/>
       </p>
-      <p>
-        <span> {{ctrl.currentHumanTime || '--:--'}} </span>
-        <span> {{ctrl.track.track_duration || '--:--'}} </span>
+      
+      <p class="TrackStatus-timeWrapper">
+        <span class="TrackStatus-currentTime"> {{ctrl.currentHumanTime || '--:--'}} </span>
+        <span class="TrackStatus-totalTime"> {{ctrl.track.track_duration || '--:--'}} </span>
       </p>
     </section>
 
