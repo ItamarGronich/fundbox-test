0fee8efad0a95aa44084de39b70adf3937c218b2
ItamarGronich <ItamarGronich@Gmail.com>
2017-06-29 11:55:03 +0300

    Controls positions flex.

--- client/app/components/player/player.component.js
+++ client/app/components/player/player.component.js
@@ -8,12 +8,17 @@ export default {
       <album-art img-url="ctrl.track.track_image_file"> </album-art>
 
       <footer class="Controls">
-        <audio-controls type="'play'"></audio-controls>
-        <audio-controls type="'pause'"></audio-controls>
-        <audio-controls type="'next'"></audio-controls>
-        <audio-controls type="'prev'"></audio-controls>
+        <section class="Controls-buttons">
+            <audio-controls type="'prev'"></audio-controls>
+            <audio-controls type="'play'"></audio-controls>
+            <audio-controls type="'pause'"></audio-controls>
+            <audio-controls type="'next'"></audio-controls>
+        </section>
+        
 
-        <track-status track="ctrl.track" current-time="ctrl.currentTime"></track-status>
+        <section class="Controls-status">
+            <track-status track="ctrl.track" current-time="ctrl.currentTime"></track-status>
+        </section>
       </footer>
     </section>
     `,
